32f37ebfc14b3d2fb2ba49ed8db2067e3c0d2bb9
chaeyn <chaulfe@gmail.com>
2026-07-25 01:33:22 +0900

    chore: init iOS

---
+++ apps/ios/InnoLive/InnoLive/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InnoLive
+//
+//  Created by chaeyn on 7/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
