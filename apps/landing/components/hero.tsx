b264994c15b81d3dcff6d40747abdce469dd709f
jdw09 <razion0304@naver.com>
2026-09-18 12:26:08 +0900

    feat: Hero 섹션 높이 조정

--- apps/landing/components/hero.tsx
+++ apps/landing/components/hero.tsx
@@ -6,10 +6,10 @@ import { TypewriterHeroHeading } from "@/components/typewriter-hero-heading";
 export function Hero() {
   return (
     <section
-      className="flex w-full flex-col items-center overflow-x-clip px-[var(--page-gutter)] pt-12 lg:min-h-[calc(100dvh-var(--header-block-desktop))] lg:justify-center lg:pt-0"
+      className="flex w-full h-screen flex-col items-center overflow-x-clip px-[var(--page-gutter)] pt-12 lg:min-h-[calc(100dvh-var(--header-block-desktop))] lg:justify-center lg:pt-0"
       aria-labelledby="hero-heading"
     >
-      <div className="flex w-full max-w-[1640px] flex-col items-center gap-y-16 lg:flex-row lg:items-center lg:justify-center lg:gap-x-16 min-[106.5rem]:gap-x-[161.5px]">
+      <div className="flex relative top-12 w-full max-w-[1640px] flex-col items-center gap-y-16 lg:flex-row lg:items-center lg:justify-center lg:gap-x-16 min-[106.5rem]:gap-x-[161.5px]">
         <div className="flex w-full min-w-0 max-w-[752px] flex-col items-start lg:translate-x-12 lg:flex-1 min-[106.5rem]:w-[752px] min-[106.5rem]:flex-none min-[106.5rem]:items-end">
           <div className="flex w-full max-w-[634px] flex-col gap-12">
             <div className="relative flex w-full flex-col gap-8">
